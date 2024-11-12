7d6352bfe2a944e97d75896b1df897e3a5e91d8b
sahil1330 <sahilmane025@gmail.com>
2024-11-12 16:31:24 +0530

    Refactor API response type and add new UI components for layout and navigation

--- src/types/ApiResponse.ts
+++ src/types/ApiResponse.ts
@@ -3,7 +3,7 @@ import { Message } from "@/model/User.model";
 export interface ApiResponse {
   success: boolean;
   message: string;
-  isAcceptingMessages?: boolean;
+  isAcceptingMessage?: boolean;
   messages?: Array<Message>;
 //   data?: any;
 }
